ac310b9ab206049ea3913b4f076b5ce4c3bbc9ff
Gabriel Cypriano <gabrielcs@gmail.com>
2013-11-13 23:43:02 -0200

    Last commit for the API logging

--- app/controllers/api/auth/APILoggingController.java
+++ app/controllers/api/auth/APILoggingController.java
@@ -31,24 +31,24 @@ public class APILoggingController extends Action<Logged> {
 
 		// builds the string
 		StringBuilder logString = new StringBuilder();
-		if (dev == null) {
-			logString.append("\n Developer: null\n       App: null");
-		} else {
-			//logString.append(String.format("\n Developer: %s\n App:       %s",
-			//		dev.getEmailAddress(), app.getName()));
-			logString.append(String.format("\n Developer: %s\n       App: %s",
-							dev.getEmailAddress(), app.getName()));
+		if (dev != null) {
+			logString.append(String.format("\n Developer: %s\n App:       %s",
+					dev.getEmailAddress(), app.getName()));
 		}
 
-		if (user == null) {
-			logString.append("\n      User: null");
-		} else {
-			logString.append(String.format("\n      User: %s (id: %s)",
+		if (user != null) {
+			logString.append(String.format("\n User:      %s (id: %s)",
 					user.getEmailAddress(), user.getId()));
 		}
 
-		logString.append(String.format("\n   Request: %s\n      Body: %s\n",
-				context.request(), context.request().body().asJson()));
+		logString.append("\n Request:   " + context.request());
+
+		if (context.request().body().asJson() != null) {
+			logString.append("\n Body:      "
+					+ context.request().body().asJson());
+		}
+		
+		logString.append("\n");
 
 		// process the log
 		MyLogger.info(logString.toString());
